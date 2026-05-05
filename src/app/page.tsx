1cdc4d11cbae692efcc0a80ee46515e769c4dccf
emmanuelomejua <omejuaifeanyi2@gmail.com>
2026-05-05 12:23:21 +0100

    implement useful primitive component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,14 @@
+import { Insight, PublicFooter, PublicHero, PublicTopbar, Solution } from "@/components/landing/LandingPage";
 
 
 export default function Home() {
   return (
     <main className="">
-
+      <PublicTopbar/>
+      <PublicHero/>
+      <Solution/>
+      <Insight/>
+      <PublicFooter/>
     </main>
   );
 }
---
+++ src/components/landing/LandingPage.tsx
@@ -0,0 +1,34 @@
+import React from 'react'
+
+export const PublicTopbar = () => {
+  return (
+    <div>LandingPage</div>
+  )
+}
+
+export const PublicHero = () => {
+  return (
+    <div>LandingPage</div>
+  )
+}
+
+
+export const Solution = () => {
+  return (
+    <div>LandingPage</div>
+  )
+}
+
+export const Insight = () => {
+  return (
+    <div>LandingPage</div>
+  )
+}
+
+
+export const PublicFooter = () => {
+  return (
+    <div>LandingPage</div>
+  )
+}
+
