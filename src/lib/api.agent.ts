3c2fb20c082004f5da438e5088bb548345e98df0
emmanuelomejua <omejuaifeanyi2@gmail.com>
2026-07-21 14:21:59 +0100

    minor API refactor

--- src/lib/api.agent.ts
+++ src/lib/api.agent.ts
@@ -1,113 +1,17 @@
-
-import { SERVER } from "@/utils/fetchUtil";
-import { getAccessToken } from "./auth.actions";
+import { getData } from "./api";
 
 
+export const agentGetMyProfile = () => getData('agents/profiles/me', { errorMessage: 'Error loading agents profile' });
 
-export const agentGetDashboardOverview = async () => {
-    const token = await getAccessToken();
+export const getAllAgentsBusinessCustomers = () => getData('business-users/all');
 
-    const res = await SERVER.get('agents/loans/dashboard/overview', {
-        revalidate: 120,
-        timeout: 5000,
-        token,
-    });
-    if(res.data === null || res.status !== 200) {
-        return {
-            error: res.error || 'Failed to fetch agents',
-            success: false,
-        }
-    }
-    return res.data;
-}
+export const getAllAgentsBusinessCustomersDetails = () => (id: string) => getData(`business-users/${id}/details`);
 
+export const agentGetOnboardingStats = () => getData('agents/onboarding/stats');
 
+ export const agentGetLoanStats = () => getData('agents/loans/dashboard/stats');
 
-export const agentGetAllLoans = async () => {
-    const token = await getAccessToken();
+ export const agentGetAllLoans = () => getData('agents/loans/all');
 
-    const res = await SERVER.get('agents/loans/all', {
-        revalidate: 120,
-        timeout: 5000,
-        token,
-    });
-    if(res.data === null || res.status !== 200) {
-        return {
-            error: res.error || 'Failed to fetch agents',
-            success: false,
-        }
-    }
-    return res.data;
-}
+ export const agentGetDashboardOverview = () => getData('agents/loans/dashboard/overview');
 
-
-export const agentGetLoanStats = async () => {
-    const token = await getAccessToken();
-
-    const res = await SERVER.get('agents/loans/dashboard/stats', {
-        revalidate: 120,
-        timeout: 5000,
-        token,
-    });
-    if(res.data === null || res.status !== 200) {
-        return {
-            error: res.error || 'Failed to fetch agents',
-            success: false,
-        }
-    }
-    return res.data;
-}
-
-
-export const agentGetOnboardingStats = async () => {
-    const token = await getAccessToken();
-
-    const res = await SERVER.get('agents/onboarding/stats', {
-        revalidate: 120,
-        timeout: 5000,
-        token,
-    });
-    if(res.data === null || res.status !== 200) {
-        return {
-            error: res.error || 'Failed to fetch agents',
-            success: false,
-        }
-    }
-    return res.data;
-}
-
-
-export const getAllAgentsBusinessCustomers = async () => {
-    const token = await getAccessToken();
-
-    const res = await SERVER.get('business-users/all', {
-        revalidate: 120,
-        timeout: 5000,
-        token,
-    });
-    if(res.data === null || res.status !== 200) {
-        return {
-            error: res.error || 'Failed to fetch agents',
-            success: false,
-        }
-    }
-    return res.data;
-}
-
-
-export const getAllAgentsBusinessCustomersDetails = async (id: string) => {
-    const token = await getAccessToken();
-
-    const res = await SERVER.get(`business-users/${id}/details`, {
-        revalidate: 120,
-        timeout: 5000,
-        token,
-    });
-    if(res.data === null || res.status !== 200) {
-        return {
-            error: res.error || 'Failed to fetch agents',
-            success: false,
-        }
-    }
-    return res.data;
-}
